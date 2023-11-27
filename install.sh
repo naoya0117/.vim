62fa1f2fe2046f6eb0b73d541eabbc6b5e082e01
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-11-27 14:43:52 +0900

    スクリプトの更新

--- install.sh
+++ install.sh
@@ -1,14 +1,56 @@
 #!/bin/bash
 
-dir_name=$(cd $(dirname $0) ; pwd | xargs basename)
+SCRIPT_DIR=$(cd $(dirname $0) ; pwd)
 
-( [[ "$dir_name" != ".vim_runtime" ]] || ! [ -d ~/.vim_runtime ] ) &&
-	echo "you need to clone vim_runtime as ~/.vim_runtime" && exit 1
+isModuleInstaled() {
+    installed=$(ls ${SCRIPT_DIR}/bundle/*/ | grep -v bundle | wc -l)
 
-! [ -f ~/.vimrc ] && touch ~/.vimrc 
-[ -n "$(grep .vim_runtime ~/.vimrc)" ] && echo "vim_runtime is already installed"
-if [ -z "$(grep .vim_runtime ~/.vimrc)" ] ; then
-	echo "source ~/.vim_runtime/init.vim" >> ~/.vimrc
-	git submodule update --init --recursive
+    if [ $installed -gt 0 ] ; then
+        echo "modules are already installed"
+        return 0
+    else
+        echo "modules are not installed"
+        return 1
+    fi
+
+}
+
+isPathCorrect() {
+    if [[ ${SCRIPT_DIR} == "${HOME}/.vim_runtime" ]] ; then
+        echo "path check is done."
+        return 0
+    fi
+    echo "path check is failed."
+    echo "you need to clone vim_runtime as ~/.vim_runtime"
+    echo "run \"git clone https://github.com/naoya0117/vim_runtime.git ~/.vim_runtime\""
+    return 1
+}
+
+isVimruntimeLoaded () {
+    if [ -n "$(grep .vim_runtime ~/.vimrc)" ] ; then
+        echo "vim_runtime is already loaded"
+        return 0
+    fi
+    return 1
+}
+
+if ! [ -f ~/.vimrc ] ; then
+    touch ~/.vimrc
 fi
 
+
+if isPathCorrect ; then
+    echo "path check is done"
+else
+    exit 1
+fi
+
+if ! isModuleInstaled  ; then
+    echo "installing modules..."
+    git submodule update --init --recursive
+fi
+
+if ! isVimruntimeLoaded ; then
+    echo "updatint vimrc..."
+    echo "source ~/.vim_runtime/init.vim" >> ~/.vimrc
+fi
